16bf1a00cef3023abee3317f4dbd28635e764332
Natalia Zaitseva <37537790+natali-zaitseva@users.noreply.github.com>
2019-10-08 11:39:55 +0300

    MODCFIELDS-17 - Custom Fields: Radio Button Definition Validation (#14)
    * added validation for the Radio button custom field definition
     * added tests

--- src/test/java/org/folio/validate/definition/RadioButtonDefinitionValidatorTest.java
+++ src/test/java/org/folio/validate/definition/RadioButtonDefinitionValidatorTest.java
@@ -17,19 +17,19 @@ import org.folio.test.util.TestUtil;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
-public class OptionSizeValidatorTest {
+public class RadioButtonDefinitionValidatorTest {
 
   @Autowired
-  private OptionSizeValidator validator;
+  private RadioButtonDefinitionValidator validator;
   @Rule
   public ExpectedException expectedEx = ExpectedException.none();
 
   @Test
-  public void shouldReturnErrorIfIncorrectNumberOfOptions() throws IOException, URISyntaxException {
+  public void shouldReturnErrorIfIncorrectNumberOfDefaults() throws IOException, URISyntaxException {
     expectedEx.expect(IllegalArgumentException.class);
-    expectedEx.expectMessage("The max option size for 'Radio button' custom field type is 5");
+    expectedEx.expectMessage("The max defaults size for 'RADIO_BUTTON' custom field type is 1");
     final CustomField customField = TestUtil.readJsonFile(
-      "fields/post/radioButton/postRadioButtonWithInvalidSizeOptions.json", CustomField.class);
+      "fields/post/radioButton/postRadioButtonWithInvalidDefaultsSize.json", CustomField.class);
     validator.validateDefinition(customField);
   }
 }
