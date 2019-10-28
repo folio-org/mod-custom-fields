31fefa13cb42dc202c49fc0938acccf41b52506e
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2019-10-28 17:26:33 +0200

    MODCFIELDS-18 refactor CF usage statistic model (#25)

--- src/main/java/org/folio/service/RecordService.java
+++ src/main/java/org/folio/service/RecordService.java
@@ -3,11 +3,11 @@ package org.folio.service;
 import io.vertx.core.Future;
 
 import org.folio.rest.jaxrs.model.CustomField;
-import org.folio.rest.jaxrs.model.CustomFieldStatisticCollection;
+import org.folio.rest.jaxrs.model.CustomFieldStatistic;
 
 public interface RecordService {
 
-  Future<CustomFieldStatisticCollection> retrieveStatistic(CustomField field, String tenantId);
+  Future<CustomFieldStatistic> retrieveStatistic(CustomField field, String tenantId);
 
   Future<Void> deleteAllValues(CustomField field, String tenantId);
 }
