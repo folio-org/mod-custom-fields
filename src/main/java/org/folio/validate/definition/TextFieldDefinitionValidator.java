ddc3c38c98cc5a763d237e52383454999061da09
Pavlo Smahin <Pavlo_Smahin@epam.com>
2020-01-29 17:48:44 +0200

    MODCFIELDS-36 Improve custom fields validation
    - add validation for TEXTBOX_SHORT and TEXTBOX_LONG
    - add validation for SINGLE_CHECKBOX
    - add tests for validators above
    - fix exists tests

--- src/main/java/org/folio/validate/definition/TextFieldDefinitionValidator.java
+++ src/main/java/org/folio/validate/definition/TextFieldDefinitionValidator.java
@@ -1,20 +1,33 @@
 package org.folio.validate.definition;
 
+import static java.util.Objects.nonNull;
+
 import static org.folio.validate.definition.AllowedFieldsConstants.TEXT_ALLOWED_FIELDS;
 
-import org.folio.rest.jaxrs.model.CustomField;
+import org.apache.commons.lang3.Validate;
 import org.springframework.stereotype.Component;
 
+import org.folio.rest.jaxrs.model.CustomField;
+
 @Component
-public class TextFieldDefinitionValidator implements Validatable{
+public class TextFieldDefinitionValidator implements Validatable {
+
+  public static final String TEXT_FIELD_DEFINING_MESSAGE = "The 'textField' property should be defined for '%s' custom field type.";
+  public static final String MAX_SIZE_NON_NULL_MESSAGE = "The value for 'maxSize' should not be null.";
+  public static final String MAX_SIZE_MUST_BE_POSITIVE_MESSAGE = "The value for 'maxSize' should be greater than 0.";
+
   @Override
   public void validateDefinition(CustomField fieldDefinition) {
     CustomDefinitionValidationUtil.onlyHasAllowedFields(fieldDefinition, TEXT_ALLOWED_FIELDS);
+
+    Validate.isTrue(nonNull(fieldDefinition.getTextField()), TEXT_FIELD_DEFINING_MESSAGE, fieldDefinition.getType());
+    Validate.isTrue(nonNull(fieldDefinition.getTextField().getMaxSize()), MAX_SIZE_NON_NULL_MESSAGE);
+    Validate.isTrue(fieldDefinition.getTextField().getMaxSize() > 0, MAX_SIZE_MUST_BE_POSITIVE_MESSAGE);
   }
 
   @Override
   public boolean isApplicable(CustomField fieldDefinition) {
     return CustomField.Type.TEXTBOX_SHORT.equals(fieldDefinition.getType()) ||
-           CustomField.Type.TEXTBOX_LONG.equals(fieldDefinition.getType());
+      CustomField.Type.TEXTBOX_LONG.equals(fieldDefinition.getType());
   }
 }
--- src/test/java/org/folio/validate/definition/CheckboxDefinitionValidatorTest.java
+++ src/test/java/org/folio/validate/definition/CheckboxDefinitionValidatorTest.java
@@ -1,12 +1,12 @@
 package org.folio.validate.definition;
 
+import static org.junit.Assert.assertTrue;
+
+import static org.folio.test.util.TestUtil.readJsonFile;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import org.folio.rest.jaxrs.model.CustomField;
-import org.folio.rest.jaxrs.model.TextField;
-import org.folio.spring.TestConfiguration;
-import org.folio.test.util.TestUtil;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,22 +15,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.folio.rest.jaxrs.model.CustomField;
+import org.folio.spring.TestConfiguration;
+
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
 public class CheckboxDefinitionValidatorTest {
 
-  @Autowired
-  private CheckboxDefinitionValidator validator;
   @Rule
   public ExpectedException expectedEx = ExpectedException.none();
+  @Autowired
+  private CheckboxDefinitionValidator validator;
+
+  @Test
+  public void shouldBeApplicableForTextBoxShort() throws IOException, URISyntaxException {
+    CustomField customField = readJsonFile("fields/post/checkbox/postCheckbox.json", CustomField.class);
+    assertTrue(validator.isApplicable(customField));
+  }
 
   @Test
   public void shouldReturnErrorIfContainsNotAllowedFields() throws IOException, URISyntaxException {
     expectedEx.expect(IllegalArgumentException.class);
     expectedEx.expectMessage("Attribute textField is not allowed");
-    final CustomField customField = TestUtil.readJsonFile(
-      "fields/post/checkbox/postCheckbox.json", CustomField.class);
-    customField.setTextField(new TextField());
+    CustomField customField = readJsonFile("fields/post/checkbox/postWithNotAllowedFields.json", CustomField.class);
+    validator.validateDefinition(customField);
+  }
+
+  @Test
+  public void shouldReturnErrorIfCheckboxIsNotDefined() throws IOException, URISyntaxException {
+    expectedEx.expect(IllegalArgumentException.class);
+    expectedEx.expectMessage("The 'checkBox' property should be defined for 'SINGLE_CHECKBOX' custom field type.");
+    CustomField customField = readJsonFile("fields/post/checkbox/postWithCheckboxNotDefined.json", CustomField.class);
     validator.validateDefinition(customField);
   }
 }
