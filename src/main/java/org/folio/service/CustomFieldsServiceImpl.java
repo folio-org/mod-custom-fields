3c218d378db4ed6fef143310e4a5136d1dedca88
Natalia Zaitseva <37537790+natali-zaitseva@users.noreply.github.com>
2020-06-25 20:39:05 +0300

    MODCFIELDS-55 - Incorrect order of Custom Fields when adding a 10th field (#50)
    * MODCFIELDS-55 - Incorrect order of Custom Fields when adding a 10th field
    
    * fixed sorting order for field with name "order"
    * fixed regex for getting maxRefId
    * added tests

--- src/main/java/org/folio/service/CustomFieldsServiceImpl.java
+++ src/main/java/org/folio/service/CustomFieldsServiceImpl.java
@@ -18,6 +18,7 @@ import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -65,8 +66,9 @@ import org.folio.service.exc.ServiceExceptions;
 public class CustomFieldsServiceImpl implements CustomFieldsService {
 
   private static final String ALL_RECORDS_QUERY = "cql.allRecords=1";
+
   private static final String FORMAT_ATTRIBUTE = "fieldFormat";
-  private static final String ORDER_ATTRIBUTE = "order";
+  private static final String ORDER_ATTRIBUTE = "order/number";
   private static final String TYPE_ATTRIBUTE = "type";
 
   private static final String TYPE_CHANGING_MESSAGE =
@@ -343,8 +345,7 @@ public class CustomFieldsServiceImpl implements CustomFieldsService {
   }
 
   private List<CustomField> updateCustomFieldsOrder(CustomFieldCollection customFieldsCollection) {
-    final List<CustomField> customFields = customFieldsCollection.getCustomFields();
-    customFields.sort(comparing(CustomField::getOrder));
+    final List<CustomField> customFields = sortByOrder(customFieldsCollection).getCustomFields();
     for (int i = 0; i < customFields.size(); i++) {
       customFields.get(i).setOrder(i + 1);
     }
@@ -410,6 +411,11 @@ public class CustomFieldsServiceImpl implements CustomFieldsService {
       throw new IllegalArgumentException("Unsupported Query Format : Search query is in an unsupported format: " + cqlQuery,
         e);
     }
+  }
+
+  private CustomFieldCollection sortByOrder(CustomFieldCollection collection) {
+    collection.getCustomFields().sort(Comparator.comparingInt(CustomField::getOrder));
+    return collection;
   }
 
   private void generateOptionIds(CustomField field) {
